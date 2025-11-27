bcbc06ff0fd4a811a069813090fcca916d46abaf
alfanovemberfox <alexfederlin@gmail.com>
2025-11-27 23:19:26 +0000

    Lösung gesichert

--- 08_Dezember/main.py
+++ 08_Dezember/main.py
@@ -2,11 +2,15 @@
 
 # 1. Erstelle eine Variable 'schlitten_bereit' und setze sie auf True.
 #    Achtung: True schreibt man groß!
-
+schlitten_bereit = True
 
 # 2. Schreibe eine if-Abfrage, die prüft ob der Schlitten bereit ist.
 #    Profi-Tipp: Du brauchst kein "== True". Schreib einfach: if schlitten_bereit:
 
     # Wenn bereit: Gib "Motoren starten!" aus.
 
+if schlitten_bereit:
+    print ("los gehts")
     # Sonst (else): Gib "Warten..." aus.
+else:
+    print ("warten")
