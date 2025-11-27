573ac9584df211ec107ecda6c1f46c0d7d4d4dc8
alfanovemberfox <alexfederlin@gmail.com>
2025-11-27 21:16:04 +0000

    Fortschritt gespeichert um 21:16:04

--- 03_Dezember/main.py
+++ 03_Dezember/main.py
@@ -4,11 +4,13 @@
 # Achtung: Zahlen schreibt man im Code OHNE Gänsefüßchen!
 
 # 1. Erstelle eine Variable 'rentiere' und setze sie auf 9.
-
+rentiere=9
 
 # 2. Erstelle eine Variable 'geschenke' und setze sie auf 0.
-
+geschenke=0
 
 # 3. Gib beide Variablen mit print() aus.
 #    (Erinnerung an Tag 2: Variable in die Klammer, ohne Anführungszeichen)
 
+print (rentiere)
+print (geschenke)
