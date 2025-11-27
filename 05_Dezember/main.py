b6c364fb5ba30251fe367cb74152d260ab4bb94f
alfanovemberfox <alexfederlin@gmail.com>
2025-11-27 21:44:21 +0000

    Lösung gesichert

--- 05_Dezember/main.py
+++ 05_Dezember/main.py
@@ -4,9 +4,10 @@ print("System bereit für Eingabe...")
 
 # 1. Nutze input(), um den User zu fragen und speichere das Ergebnis in der Variable 'wunsch'.
 #    Schreibe deine Frage als Text in die Klammern von input("...").
-
+wunsch = input ("Was wünscht du dir? ")
 
 # 2. Gib den gespeicherten Wunsch zur Bestätigung aus.
 print("Du wünschst dir:")
 #    Gib hier (wie an Tag 2) den Inhalt der Variable 'wunsch' aus.
 
+print (wunsch)
