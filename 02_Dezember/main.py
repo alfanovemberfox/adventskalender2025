d25ba80c6bdc29228f0b83c0b93155105febdc3b
alfanovemberfox <alexfederlin@gmail.com>
2025-11-29 23:31:02 +0000

    Lösung gesichert

--- 02_Dezember/main.py
+++ 02_Dezember/main.py
@@ -3,9 +3,10 @@
 # 1. Erstelle eine Variable mit dem Namen 'elfen_name'.
 #    Speichere darin einen Namen (in Anführungszeichen, z.B. "Glitzer").
 #    Tipp: variable = "Wert"
-
+bla="blub"
 
 # 2. Gib den Inhalt der Variable auf dem Bildschirm aus.
 #    Nutze dafür den print() Befehl mit dem Variablennamen.
 #    WICHTIG: Hier KEINE Anführungszeichen, sonst druckt er nur das Wort "elfen_name" statt des Inhalts!
+print (bla)
 
